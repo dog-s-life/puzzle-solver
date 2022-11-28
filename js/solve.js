352ed5c5575ab46ba85d2974101e23e2664991b1
Dog's life <55963534+dog-s-life@users.noreply.github.com>
2022-11-28 21:45:57 +0900

    Add files via upload

--- js/solve.js
+++ js/solve.js
@@ -1,37 +1,37 @@
 function left(str, size) {
-    const index = str.indexOf('X');
+    const index = str.indexOf('x');
     if (index % size === 0) {
         return null;
     }
 
-    return str.substring(0, index - 1) + 'X' + str.charAt(index - 1) + str.substring(index + 1);
+    return str.substring(0, index - 1) + 'x' + str.charAt(index - 1) + str.substring(index + 1);
 }
 
 function right(str, size) {
-    const index = str.indexOf('X');
+    const index = str.indexOf('x');
     if (index % size === size - 1) {
         return null;
     }
 
-    return str.substring(0, index) + str.charAt(index + 1) + 'X' + str.substring(index + 2);
+    return str.substring(0, index) + str.charAt(index + 1) + 'x' + str.substring(index + 2);
 }
 
 function up(str, size) {
-    const index = str.indexOf('X');
+    const index = str.indexOf('x');
     if (Math.floor(index / size) === 0) {
         return null;
     }
 
-    return str.substring(0, index - size) + 'X' + str.substring(index - size + 1, index) + str.charAt(index - size) + str.substring(index + 1);
+    return str.substring(0, index - size) + 'x' + str.substring(index - size + 1, index) + str.charAt(index - size) + str.substring(index + 1);
 }
 
 function down(str, size) {
-    const index = str.indexOf('X');
+    const index = str.indexOf('x');
     if (Math.floor(index / size) === size - 1) {
         return null;
     }
 
-    return str.substring(0, index) + str.charAt(index + size) + str.substring(index + 1, index + size) + 'X' + str.substring(index + size + 1);
+    return str.substring(0, index) + str.charAt(index + size) + str.substring(index + 1, index + size) + 'x' + str.substring(index + size + 1);
 }
 
 let results = [];
@@ -104,7 +104,7 @@ function solve(input, goal, maxTrial = 30) {
 self.onmessage = e  => {
     const { quiz, goal, maxTrial } = e.data;
 
-    const result = solve(quiz, goal, maxTrial);
+    const result = solve(quiz.toLowerCase(), goal.toLowerCase(), maxTrial);
 
     self.postMessage(result);
 };
